78971d0fc0bf8fa2e357bed109fa66977838478f
Magi <xchxch1996@gmail.com>
2019-10-02 00:22:37 +0800

    updated

--- app/src/main/java/com/magi/imoocrestaurant/ui/activity/OrderActivity.java
+++ app/src/main/java/com/magi/imoocrestaurant/ui/activity/OrderActivity.java
@@ -1,6 +1,6 @@
 package com.magi.imoocrestaurant.ui.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.magi.imoocrestaurant.R;
